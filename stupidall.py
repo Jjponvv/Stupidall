6e4dd5fc5bfc4340976f63c3d34d68b488424bb4
Jjponv <99134989+Jjponvv@users.noreply.github.com>
2024-11-15 20:18:24 +0200

    sprint added

--- stupidall.py
+++ stupidall.py
@@ -1,6 +1,20 @@
+import string, random, time, os
 
 
-
-class print:
+class sprint:
     def __init__(self, text):
-        self.text = text
+        symbols = string.printable
+        output = ''
+        for i in text:
+            while True:
+                rch = random.choice(symbols)
+                output += rch
+                print(output, end='\r', flush=True)
+                time.sleep(0.05)
+                if rch == i:
+                    break
+                else:
+                    output = output[:-1]
+                    os.system('cls')
+                     
+        print(output)
